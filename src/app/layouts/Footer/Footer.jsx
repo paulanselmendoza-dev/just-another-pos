1bf7140c1b344e24f1e998a614237ec73777b8bf
sprezza <paulanselmendoza@gmail.com>
2026-04-01 22:54:55 +0800

    remake with typescript

--- src/app/layouts/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import "./Footer.scss";
-
-function Footer() {
-    return <div className="footer">Footer</div>;
-}
-
-export default Footer;
--- src/assets/icons/icons.jsx
+++
@@ -1,5 +0,0 @@
-export { default as AccountIcon } from "./AccountIcon";
-export { default as CartIcon } from "./CartIcon";
-export { default as HamburgerIcon } from "./HamburgerIcon";
-export { default as SearchIcon } from "./SearchIcon";
-export { default as XmarkIcon } from "./XmarkIcon";
